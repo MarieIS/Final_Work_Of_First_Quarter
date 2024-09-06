848120bf951961188cb235e0a8742d12a5e04173
Maria <mariya-ivanova@list.ru>
2024-09-06 21:23:59 +0200

    The solution is ready

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿void PrintArray(string[] array)
+{
+  if (array.Length == 0)
+   {
+     Console.WriteLine("The array is empty!");
+   }
+   else
+   {
+     Console.WriteLine("The array: ");
+     Array.ForEach(array, (str) => Console.Write($"{str} "));
+     Console.WriteLine();
+   } 
+}
+
+string[] GetNewArray(string[] array)
+{
+   int length = array.Length;
+   string[] newarr = new string[length];   
+   int count = 0;
+   for (int i = 0; i < length; i++)
+   {
+      if (array[i].Length <= 3)
+       {
+         newarr[count] = array[i];
+         count++;
+       }
+   }
+   Array.Resize(ref newarr, count);
+   return newarr;
+}
+
+string [] GetAnArray(int size) {
+   string [] array = new string[size];
+   for (int i = 0; i < size; i++){
+    Console.Write("Insert an element: ");
+    array[i] = Console.ReadLine();
+   }
+   return array;
+}
+
+int GetNumberAmount(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number1 = GetNumberAmount("Введите желаемое количество элементов массива: ");
+string [] arr1 = GetAnArray(number1);
+PrintArray(arr1);
+string [] arr2 = GetNewArray(arr1);
+PrintArray(arr2);
